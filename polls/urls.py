604d4152bee000e130dc945d7dc0a25a72c49523
franztscharf <tscharf@campus.tu-berlin.de>
2016-05-08 13:52:24 +0200

    added all functionality but still we need to clean UP the code it's pretty messy!

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,9 @@ urlpatterns = patterns('',
     url(r'^overview/$', views.overview, name='overview'),
     url(r'^new/$', views.new, name='new'),
     url(r'^edit/$', views.edit, name='edit'),
+    url(r'^overEdit/$', views.overEdit, name='overEdit'),
+    url(r'^update/$', views.update, name='update'),
+    url(r'^delete/$', views.delete, name='delete'),
     url(r'^imprint/$', views.imprint, name='imprint'),
+
 )
--- polls/views.py
+++ polls/views.py
@@ -12,6 +12,32 @@ from .models import ToDo
 def index(request):
     return render(request, 'polls/index.html', {})
 
+def delete(request):
+    if request.method == "POST":
+        todo = ToDo.objects.get(id=request.POST['id'])
+        todo.delete()
+        return redirect('/overview/')
+    else:
+        return render(request, 'polls/content/edit.html', {}) 
+
+def update(request):
+    if request.method == "POST":
+        todo = ToDo.objects.get(id=request.POST['id'])
+        todo.title = request.POST['title']
+        todo.name=request.POST['name']
+        todo.description=request.POST['description']
+        todo.deadline_day=request.POST['day']
+        todo.deadline_month=request.POST['month']
+        todo.deadline_year=request.POST['year']
+        progressFromPost = request.POST['progress']
+        progressToNum = progressFromPost.split("%")
+        todo.progress=progressToNum[0]
+        todo.priority=2
+        todo.save()
+        return redirect('/overview/')
+    else:
+        return render(request, 'polls/content/edit.html', {}) 
+
 def overview(request):
 	#highest prio should be first element in list
     todos = ToDo.objects.order_by('priority').reverse
@@ -33,7 +59,13 @@ def new(request):
         form = ToDoForm()
         return render(request, 'polls/content/new.html', {'form': form})
        
-
+def overEdit(request):
+    if request.method == "POST":
+        todo = ToDo.objects.get(id=request.POST['id'])
+        return render(request, 'polls/content/edit.html', {'todo': todo})
+    else:
+        return render(request, 'polls/content/overview.html', {'todos': todos})
+        
 def edit(request):
     form = ToDoForm()
     if request.method == "POST":
